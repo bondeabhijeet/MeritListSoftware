648e10108401efc442c1a8650bbca2c962c21ae0
bondeabhijeet <bondeabhijeetunderveil@gmail.com>
2020-03-28 12:24:56 +0100

    SORTING DONE

--- trial5.c
+++ trial5.c
@@ -6,20 +6,22 @@
 struct list *create_list (void);
 //int display(struct list *record, int *index);
 int display(struct list *record, int *index);
+int *sort_on_marks(struct list *start, int choice);
 
 
 struct list
 {
     int index;
-    char f_name[40];
-    char m_name[40];
-    char l_name[40];
+    char f_name[20];
+    char m_name[20];
+    char l_name[20];
     int m1;
     int m2;
     int m3;
     int m4;
     int m5;
     int total_marks;
+    float percentage;
     struct list *next;
 
 };
@@ -58,6 +60,7 @@ struct list *create_list (void)  // CREATING A LINKED LIST
         fscanf(fp, "%d ", &record->m5);
         // CALCULATING TOTAL MARKS AND PLACING IT IN ITS RESPECTIVE PLACE IN THE STRUCTURE
         record->total_marks = (record->m1+record->m2+record->m3+record->m4+record->m5);
+        record->percentage = (((float)record->total_marks)/5.0);
 
         //printf("NODE CREATED %d \n", record->index); // CHECCKING WHETHER THE NODE IS CREATED OR NOT
 
@@ -99,22 +102,24 @@ int display(struct list *record, int *index)
     printf("records=%d\n", no_of_elements);  // VERIFYING THE NUMBER OF RECORDS
 
 
-    for(i=1; i<=no_of_elements; ++i)  // PRINTING THE RECORDS INDEX WISE
+    for(i=0; i<no_of_elements; ++i)  // PRINTING THE RECORDS INDEX WISE
     {
         record=start;
         do
         {
             if(record->index == *(index+i))  // PRINTING THE RECORD AT I'TH INDEX
             {
-                printf("%d ", record->index);
-                printf("%s ", record->f_name);
-                printf("%s ", record->m_name);
-                printf("%s ", record->l_name);
-                printf("%d ", record->m1);
-                printf("%d ", record->m2);
-                printf("%d ", record->m3);
-                printf("%d ", record->m4);
-                printf("%d ", record->m5);
+                printf("%3d   ", record->index);
+                printf("%-20s ", record->f_name);
+                printf("%-20s ", record->m_name);
+                printf("%-20s ", record->l_name);
+                printf("%-4d ", record->m1);
+                printf("%-4d ", record->m2);
+                printf("%-4d ", record->m3);
+                printf("%-4d ", record->m4);
+                printf("%-4d ", record->m5);
+                printf("%-5d ", record->total_marks);
+                printf("%-5.3f ", record->percentage);
                 printf("\n");
                 break;
             }
@@ -128,23 +133,105 @@ int display(struct list *record, int *index)
 }
 
 
+int *sort_on_marks(struct list *start, int choice)
+{
+    struct list *record;
+    int no_of_elements, i, j, temp, *index;
+
+    record=start;
+
+    no_of_elements=0;  // COUNTING NUMBER OF ELEMENTS
+    do
+    {
+        no_of_elements++;
+
+        record=record->next;  // INCREMENTING THE POINTER TO COUNT THE NUMBER OF ELEMENTS
+
+    }while(record!=NULL);
+
+    int marks[no_of_elements];
+    index = (int *)malloc(sizeof(int)*(no_of_elements));
+
+    for(i=0; i<no_of_elements; ++i)
+    {
+        index[i] = (i+1);
+    }
+
+    record = start;
+
+    for(i=0; i<no_of_elements; ++i)
+    {
+        if(choice == 1)
+        {
+            marks[i]= record->m1;
+        }
+        if(choice == 2)
+        {
+            marks[i]= record->m2;
+        }
+        if(choice == 3)
+        {
+            marks[i]= record->m3;
+        }
+        if(choice == 4)
+        {
+            marks[i]= record->m4;
+        }
+        if(choice == 5)
+        {
+            marks[i]= record->m5;
+        }
+        if(choice == 6)
+        {
+            marks[i]= record->total_marks;
+        }
+        if(choice == 7)
+        {
+            marks[i]= record->index;
+        }
+        record = record->next;
+    }
+    // SORTING STARTS FROM HERE
+
+
+
+    for(i=0; i<(no_of_elements-1); i++)
+    {
+        for(j=0; j<(no_of_elements-i-1); j++)
+        {
+            if(marks[j]<marks[j+1])
+            {
+                temp=marks[j];
+                marks[j]=marks[j+1];
+                marks[j+1]=temp;
+
+                temp=index[j];
+                index[j]=index[j+1];
+                index[j+1]=temp;
+            }
+        }
+    }
+
+
+    return(index);
+}
 
 
 
 
 void main(void)
 {
-    struct list *start, *n_start;
-    int l_index[101], i;
+    struct list *start;
+    int l_index[101], i, choice, *n_index;
 
 
     start = (struct list *)create_list();
-    for(i=1; i<=70; ++i)
-    {
-        l_index[i] = (70-i+1);
-    }
-    display(start, l_index);
 
+    //printf("ENTER 1 TO SORT ")
+    //scanf("%d", &choice);
+    choice=1;
+    n_index  = sort_on_marks(start, choice);
+    display(start, n_index);
 }
 
 
